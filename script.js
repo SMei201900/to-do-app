42f62f2ee732bace9d40711b125482a2ebf838b7
SMei201900 <smei201900@gmail.com>
2025-03-02 00:59:18 -0500

    created a regular html file for storing some comments /explanation of my code

--- script.js
+++ script.js
@@ -14,25 +14,12 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        /*        */
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
     }
     /* 
-
-    let li = document.createElement("li"); 
-        "let" is how we declare a variable in JS 
-        "li" here is therefore a variable 
-        document.createElement("li") means I am creating a new list item (a list is an element) 
-            It is creating one HTML element with the tag name "li"
-            It is storing this element in this "li" variable 
-
-    li.innerHTML = inputBox.addEventListener; 
-        The text that we added into the input field is inputBox.value
-        We are storing that into li.innerHTML 
-
-    listContainer.appendChild(li);
-        listContainer is referencing the id found in the HTML document 
-        appendChild adds the newly created <li> as a child to the listContainer element 
-            We want it to be displayed under the section with the id "list-container" 
-    */
+    
     inputBox.value = ""; /* Allows search box to be empty */
 
 }
