6796f9c92b9991e2ae48ce4ca6b44e1e250b1c34
SMei201900 <smei201900@gmail.com>
2025-03-02 00:55:16 -0500

    edited some comments

--- script.js
+++ script.js
@@ -16,32 +16,24 @@ function addTask() {
         listContainer.appendChild(li);
     }
     /* 
-    "let" is how we declare a variable in JS 
-    "li" here is therefore a variable 
-    document.createElement("li") means I am creating a new list item (a list is an element) 
-    
-    li.innerHTML refers to the content inside the <li> element 
-    inputBox.addEventListener is written so that the contents of the li can be changed
 
-    listContainer is referencing the id found in the HTML document 
-    appendChild adds the newly created <li> as a child to the listContainer element 
-    THUS allowing it to be displayed
-    
-    */
-    
-    /*
     let li = document.createElement("li"); 
-        It is creating one HTML element with the tag name "li" 
-        It is storing this element in this "li" variable 
+        "let" is how we declare a variable in JS 
+        "li" here is therefore a variable 
+        document.createElement("li") means I am creating a new list item (a list is an element) 
+            It is creating one HTML element with the tag name "li"
+            It is storing this element in this "li" variable 
 
     li.innerHTML = inputBox.addEventListener; 
-        The text that we added into the input field is inputBox.value 
+        The text that we added into the input field is inputBox.value
         We are storing that into li.innerHTML 
-    
+
     listContainer.appendChild(li);
-        We have to display this li
-        We want it to be displayed under the section with the id "list-container" 
+        listContainer is referencing the id found in the HTML document 
+        appendChild adds the newly created <li> as a child to the listContainer element 
+            We want it to be displayed under the section with the id "list-container" 
     */
+
 }
 
 
