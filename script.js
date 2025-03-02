ee841d750a31cd727d927416843411b2f5459b7d
SMei201900 <smei201900@gmail.com>
2025-03-02 00:46:32 -0500

    adding what happens /what action the webpage will take when we click on the button

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box");
+/*create a variable named "input box" --> The variable is constant aka unchanging. 
+    In the (HTML) document find the element by ID thus  "getElementById"
+*/
+
+const listContainer = document.getElementById("list-container");
+
+//addTask is the name of a function
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Error! You must write something!");
+    }                       //if inputBox is empty then pop-up "You must write something" 
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.addEventListener;
+        listContainer.appendChild(li);
+    }
+    /* 
+    "let" is how we declare a variable in JS 
+    "li" here is therefore a variable 
+    document.createElement("li") means I am creating a new list item (a list is an element) 
+    
+    li.innerHTML refers to the content inside the <li> element 
+    inputBox.addEventListener is written so that the contents of the li can be changed
+    HOWEVER addEventListener is a function so we are still missing something
+
+    listContainer is referencing the id found in the HTML document 
+    appendChild adds the newly created <li> as a child to the listContainer element 
+    THUS allowing it to be displayed
+    
+    */
+}
+
+
+
